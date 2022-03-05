009f2a9474f3668754ecb94a6f1a8ddfd77e41bd
Alexy de Almeida <alexx.da95@gmail.com>
2022-03-05 21:04:07 +0000

    Co-authored-by: ZeTheNerd <ZeTheNerd@users.noreply.github.com>

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,3 +1,4 @@
+
 /*
 
 N = R*C
@@ -37,128 +38,148 @@ PIECE PIECE   A   B   x
 
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <array>
+#include <chrono>
+#include <map>
 
 using namespace std;
 
-vector<vector<int>> puzzle[51][51];
-int R, C;
+// VARIABLES ---------------------------------------------------------------------- //
 
-void insert(set_pieces, row, column)
+typedef struct piece_t
 {
-    // se chegou ao fim da linha, muda de linha
-    if (column == C - 1)
-    {
-        row++;
-        column = 0;
-    }
+    int id;
+    array<int, 4> sides;
+    bool visited;
+    bool operator<(const piece_t &other) const { return id < other.id; }
+} piece;
 
-    if (row == R - 1 && column == C - 1)
-    {
-        // colocar última peça
-        return;
-    }
+array<array<piece, 50>, 50> puzzle;
+map<piece, pair<vector<piece>, vector<piece>>> dic;
+int R, C, F;
 
-    pieceA = puzzle[row][column - 1]; // a peça à ESQUERDA da que quero inserir
-    pieceB = puzzle[row][column];     // a que quero INSERIR
-    pieceC = puzzle[row - 1][column]; // a peça EM CIMA da que quero inserir
-    int upper_left = pieceB[0];
-    int upper_right = pieceB[1];
-    int bottom_left = pieceB[3];
+// --------------------------------------------------------------------------------- //
 
+bool check_horizontal_pair(piece p1, piece p2)
+{
     int counter = 1;
-    if (row == 0)
+    int side1 = p2.sides[0], side2 = p2.sides[1], side3 = p2.sides[2], side4 = p2.sides[3];
+    // rotates p2
+    while (p1.sides[1] != side1 || p1.sides[2] != side4)
     {
-        while (upper_left != pieceA[1] && down_left != pieceA[2])
-        {
-            if (counter == 4)
-                if (column != 1)
-                    return insert(set_pieces, row, column - 1);
-                else
-                {
-                    cout << "impossible puzzle!" << endl;
-                    return;
-                }
-            // roda a peça
-            upper_left = pieceB[0 + counter % 4];
-            bottom_left = pieceB[3 + counter % 4];
-            counter++;
+        
+        if (counter == 4){
+            return false;
         }
+        side1 = p2.sides[(0 + counter) % 4];
+        side2 = p2.sides[(1 + counter) % 4];
+        side3 = p2.sides[(2 + counter) % 4];
+        side4 = p2.sides[(3 + counter) % 4];
+        counter++;
     }
+    p2.sides[0] = side1, p2.sides[1] = side2, p2.sides[2] = side3, p2.sides[3] = side4;
+    return true;
+}
 
-    else
+bool check_vertical_pair(piece p1, piece p2)
+{
+    int counter = 1;
+    int side1 = p2.sides[0], side2 = p2.sides[1], side3 = p2.sides[2], side4 = p2.sides[3];
+    // rotates p2
+    while (p1.sides[3] != side1 || p1.sides[2] != side2)
     {
-        if (column == 0)
-        {
-            while (upper_left != pieceC[3] && upper_right != pieceC[2])
-            {
-                if (counter == 4)
-                    return insert(set_pieces, row - 1, C);
-                // roda a peça
-                upper_left = pieceB[0 + counter % 4];
-                upper_right = pieceB[1 + counter % 4];
-                counter++;
+        if (counter == 4){
+            return false;
+        }
+        side1 = p2.sides[(0 + counter) % 4];
+        side2 = p2.sides[(1 + counter) % 4];
+        side3 = p2.sides[(2 + counter) % 4];
+        side4 = p2.sides[(3 + counter) % 4];
+        counter++;
+        
+    }
+    p2.sides[0] = side1, p2.sides[1] = side2, p2.sides[2] = side3, p2.sides[3] = side4;
+    return true;
+}
+
+void add_pair(int side, piece p1, piece p2)
+{
+    // combines with left piece
+    if (side == 0){
+        dic[p1].first.push_back(p2);
+    }
+    // combines with upper piece
+    if (side == 1){
+        dic[p1].second.push_back(p2);
+    }
+}
+
+void add_values()
+{
+    for (auto const &[key, val] : dic)
+    {
+        for (auto const &[key2, val2] : dic){
+            if (key.id == key2.id || key2.id == 0) {
+                continue;
+            }
+            if (check_horizontal_pair(key, key2) == true) {
+                add_pair(0, key, key2);
+            }
+            if (check_vertical_pair(key, key2) == true) {
+                add_pair(1, key, key2);
             }
         }
-
-        else
-        {
-            while ((upper_left != pieceC[4] || upper_left != pieceA[1]) && upper_right != pieceC[3] && bottom_left != pieceA[3])
-            {
-                if (counter == 4)
-                    return insert(set_pieces, row - 1, column - 1);
-                // roda a peça
-                upper_left = pieceB[0 + counter % 4];
-                upper_right = pieceB[1 + counter % 4];
-                bottom_left = pieceB[3 + counter % 4];
-                counter++;
-            }
-        }
     }
-
-    // se números coincidem. chama recursivamente
-    insert(set_pieces, row + 1, column + 1);
-
-    // acrescentar peça ao tabuleiro
-    puzzle[r][c] = index da peça;
 }
 
 int main()
 {
-    int n_test_cases, N, color;
-    vector<int> piece;
-    vector<vector<int>> set_pieces;
-
+    int n_test_cases, N;
     cin >> n_test_cases;
 
     for (int i = 0; i < n_test_cases; i++)
     {
-
-        // leitura de dados
         cin >> N >> R >> C;
 
         for (int j = 0; j < N; j++)
         {
-            for (int k = 0; k < 4; k++)
-            {
-                cin >> color;
-                piece.push_back(color);
-            }
-            set_pieces.push_back(piece);
+            piece p;
+            p.visited = false;
+            p.id = j;
+            cin >> p.sides[0] >> p.sides[1] >> p.sides[2] >> p.sides[3];
+            dic[p] = make_pair(vector<piece>(), vector<piece>());
 
             // coloca a primeira peça no puzzle
             if (j == 0)
-                puzzle[0][0] = set_pieces[0];
+                puzzle[0][0] = p;
         }
 
-        // chamar recursão aqui para cada caso de teste
-        int row = 0;
-        int column = 1;
-        insert(set_pieces, row, column);
+        add_values();
 
-        // isto é para verificar se um index já foi colocado no tabuleiro, não sei se
-        // podemos otimizar isto melhor
-        bool piece_exists_in_puzzle = find(begin(puzzle), end(puzzle), piece) != end(puzzle);
+        // print of dictionary
+        for (auto const &[key, val] : dic)
+        {
+            cout << "Key: " << key.sides[0] << key.sides[1] << key.sides[2] << key.sides[3] << endl;
+            cout << endl;
+
+            cout << "1st Values: ";
+            for (auto p: val.first){
+                    cout << " --- ";
+                    for (int i = 0; i < 4; i++) cout << p.sides[i] ; 
+                }
+
+            cout << endl;
+            cout << "2nd Values: ";
+            for (auto p: val.second){
+                    cout << " --- ";
+                    for (int i = 0; i < 4; i++) cout << p.sides[i];
+                }
+            cout << endl << endl;
+        }
+
+        dic.clear();
     }
     return 0;
 }
